d74f181ca5a2375bb815f753cdb13b892e206193
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-04-09 22:23:01 -0300

    🐛 bugfix/corringo-logica: corrigindo erro de lógica na questão 18.

--- atividade-01/question_18.sql
+++ atividade-01/question_18.sql
@@ -1,7 +1,10 @@
 /*  O número do apartamento mais caro ocupado pelo João.*/
-SELECT num from apto where cod_cat in
-  (SELECT cod_cat from cat where preco in 
-      (SELECT MAX(preco) from cat WHERE cod_cat IN 
-          (SELECT cod_cat from cat WHERE num IN 
-              (SELECT num from hospedagem WHERE cod_hosp IN 
-                  (SELECT cod_hosp from hospede where nome = 'JOÃO')))));
+select num 
+from hospede h, hospedagem ho 
+where h.nome ilike 'joão%' and h.cod_hosp = ho.cod_hosp and ho.num in  
+	(SELECT num from apto where cod_cat in
+	  (SELECT cod_cat from cat where preco in 
+	      (SELECT MAX(preco) from cat WHERE cod_cat IN 
+	          (SELECT cod_cat from apto WHERE num IN 
+	              (SELECT num from hospedagem WHERE cod_hosp IN 
+	                  (SELECT cod_hosp from hospede where nome ilike 'JOÃO%'))))));
