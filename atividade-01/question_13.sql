4d56e90f718831c4bdc4b71d8034eafe0b76d8f2
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-04-12 17:06:25 -0300

    🐛 bugfix/erro-digitação: corrigindo erro de digitação do "varchar".

--- atividade-01/question_13.sql
+++ atividade-01/question_13.sql
@@ -1,6 +1,6 @@
 /* Altere a tabela hóspede, acresentando o campo "Nacionalidade". */
 ALTER TABLE hospede
-	ADD nacionalidade VARCHAR();
+	ADD nacionalidade VARCHAR;
     
 ALTER TABLE hospede
-	ADD nacionalidade VARCHAR() DEFAULT 'Brasileiro';
+	ADD nacionalidade VARCHAR DEFAULT 'Brasileiro';
